fb93a1edc809586f8533088da744864bbaab8794
Geyser Ramírez <geyser.grs@gmail.com>
2023-07-26 12:38:41 -0600

    clean

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 const Tabs = () => {
   return (
